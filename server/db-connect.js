76f26943a2b21f9129d28a04066ca9df2211d748
samuel-fringeli <samuel.fringeli@me.com>
2021-10-05 15:56:19 +0200

    Update db-connect.js

--- server/db-connect.js
+++ server/db-connect.js
@@ -2,8 +2,8 @@ let mysql = require('mysql');
 
 module.exports = mysql.createPool({
     connectionLimit : 10,
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    host     : process.env.DB_HOST || 'dbhost',
+    user     : process.env.DB_USER || 'root',
+    password : process.env.DB_PASSWORD || 'root',
+    database : process.env.DB_NAME || 'appstore'
 });
