832b91daae7c6767a2e69d8eb9f27f06eabca133
Samuel Fringeli <samuel.fringeli@me.com>
2021-10-08 09:52:47 +0200

    improve docker images

---
+++ client/build/precache-manifest.f2b591c758e47d1116a969ce34ab312a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866482afb87510a25126aed8889405a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4fb227f6a4b597597d",
+    "url": "/static/css/main.7d8c4059.chunk.css"
+  },
+  {
+    "revision": "a2c4eff955de4653f796",
+    "url": "/static/js/2.bd5ca457.chunk.js"
+  },
+  {
+    "revision": "6e4fb227f6a4b597597d",
+    "url": "/static/js/main.df23ee01.chunk.js"
+  },
+  {
+    "revision": "7e2d3fac68ca73f29f3d",
+    "url": "/static/js/runtime-main.62643e56.js"
+  }
+]);
